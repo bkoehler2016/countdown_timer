e2b8356de1ca5ef9a1c3084ebf961e559df2c128
Benjamin Koehler <bkoehler751@gmail.com>
2022-02-21 05:49:35 -0700

    updated comments to match id name

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="demo"
+  // Output the result in an element with id="days"
   document.getElementById("days").innerHTML =
     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
