5c25538b49ee88dcc72076a505d56a34b40cb901
Benjamin Koehler <bkoehler751@gmail.com>
2022-02-21 05:34:21 -0700

    updated text

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minsEl = document.getElementById('mins');
-const secondsEl = document.getElementById('seconds');
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 22, 2022 10:00:00").getTime();
 
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
 
-const releaseYears = '22 Feb 2022';
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-function countdown() {
-    const releaseYearsDate = new Date(releaseYears);
-    const currentDate = new Date();
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    const totalSeconds = new Date(releaseYearsDate - currentDate) / 1000;
+  // Output the result in an element with id="demo"
+  document.getElementById("days").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor (totalSeconds) % 60;
-
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime (seconds);
-
-}
-
-function formatTime(time) {
-    return time < 10 ? (`0${time}`) : time;
-}
-
-//intial call
-countdown();
-
-setInterval(countdown, 1000)
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "EXPIRED";
+  }
+}, 1000);
